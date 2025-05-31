83453bafefc1e1e642b64de98ad4d76f1f8b84f2
Захаров Павел <pzakharov83@gmail.com>
2025-05-31 20:02:19 +0300

    lineup libs; remove line breaks

--- app/src/main/java/ru/yandex/buggyweatherapp/model/Location.kt
+++ app/src/main/java/ru/yandex/buggyweatherapp/model/Location.kt
@@ -3,9 +3,9 @@ package ru.yandex.buggyweatherapp.model
 data class Location(
     val latitude: Double,
     val longitude: Double,
-    val name: String? = null
+    val name: String? = null,
 ) {
-    
+
     override fun toString(): String {
         var result = ""
         result += "Latitude: $latitude, "
@@ -15,10 +15,20 @@ data class Location(
         }
         return result
     }
-    
-    
+
     override fun equals(other: Any?): Boolean {
+        if (this === other) return true
         if (other !is Location) return false
-        return latitude == other.latitude && longitude == other.longitude
+
+        if (latitude != other.latitude) return false
+        if (longitude != other.longitude) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = latitude.hashCode()
+        result = 31 * result + longitude.hashCode()
+        return result
     }
 }
--- app/src/main/java/ru/yandex/buggyweatherapp/repository/LocationRepository.kt
+++ app/src/main/java/ru/yandex/buggyweatherapp/repository/LocationRepository.kt
@@ -15,102 +15,70 @@ import ru.yandex.buggyweatherapp.utils.LocationTracker
 import java.util.Locale
 
 class LocationRepository(
-    
-    private val context: Context
+    private val context: Context,
 ) {
-    
-    private val fusedLocationClient: FusedLocationProviderClient = 
+    private val fusedLocationClient: FusedLocationProviderClient =
         LocationServices.getFusedLocationProviderClient(context)
-    
-    
     private var currentLocation: Location? = null
-    
-    
     private var locationCallback: ((Location?) -> Unit)? = null
-    
-    
+
     fun getCurrentLocation(callback: (Location?) -> Unit) {
         try {
             locationCallback = callback
-            
-            
-            fusedLocationClient.lastLocation
-                .addOnSuccessListener { location ->
-                    if (location != null) {
-                        val userLocation = Location(
-                            latitude = location.latitude,
-                            longitude = location.longitude
-                        )
-                        currentLocation = userLocation
-                        callback(userLocation)
-                    } else {
-                        
-                        requestLocationUpdates(callback)
-                    }
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    val userLocation = Location(
+                        latitude = location.latitude, longitude = location.longitude
+                    )
+                    currentLocation = userLocation
+                    callback(userLocation)
+                } else {
+                    requestLocationUpdates(callback)
                 }
-                .addOnFailureListener { e ->
-                    Log.e("LocationRepository", "Error getting location", e)
-                    callback(null)
-                }
+            }.addOnFailureListener { e ->
+                Log.e("LocationRepository", "Error getting location", e)
+                callback(null)
+            }
         } catch (e: SecurityException) {
             Log.e("LocationRepository", "Location permission not granted", e)
             callback(null)
         }
     }
-    
-    
+
+
     private fun requestLocationUpdates(callback: (Location?) -> Unit) {
         try {
             val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 10000)
-                .setWaitForAccurateLocation(false)
-                .setMinUpdateIntervalMillis(5000)
-                .build()
-            
+                .setWaitForAccurateLocation(false).setMinUpdateIntervalMillis(5000).build()
             val locationCallback = object : LocationCallback() {
                 override fun onLocationResult(locationResult: LocationResult) {
                     locationResult.lastLocation?.let { location ->
                         val userLocation = Location(
-                            latitude = location.latitude,
-                            longitude = location.longitude
+                            latitude = location.latitude, longitude = location.longitude
                         )
                         currentLocation = userLocation
                         callback(userLocation)
-                        
-                        
                     }
                 }
             }
-            
-            
             fusedLocationClient.requestLocationUpdates(
-                locationRequest,
-                locationCallback,
-                Looper.getMainLooper()
+                locationRequest, locationCallback, Looper.getMainLooper()
             )
         } catch (e: SecurityException) {
             Log.e("LocationRepository", "Location permission not granted", e)
             callback(null)
         }
     }
-    
-    
+
+
     fun getCityNameFromLocation(location: Location): String? {
         try {
-            
             val geocoder = Geocoder(context, Locale.getDefault())
-            
-            @Suppress("DEPRECATION")
-            val addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1)
-            
+            @Suppress("DEPRECATION") val addresses =
+                geocoder.getFromLocation(location.latitude, location.longitude, 1)
             return if (!addresses.isNullOrEmpty()) {
                 val address = addresses[0]
-                if (address.locality != null) {
-                    address.locality
-                } else if (address.subAdminArea != null) {
-                    address.subAdminArea
-                } else {
-                    address.adminArea
-                }
+                address.locality ?: (address.subAdminArea ?: address.adminArea)
             } else {
                 null
             }
@@ -119,11 +87,8 @@ class LocationRepository(
             return null
         }
     }
-    
-    
+
     fun startLocationTracking() {
         LocationTracker.getInstance(context).startTracking()
     }
-    
-    
 }
